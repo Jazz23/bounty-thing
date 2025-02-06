6f8b67a47f670a67d1a0c8d590220a0f0025b41b
Jazz23 <noreply>
2025-02-06 19:46:41 +0000

    chat bubble wip

--- bounty-thing/src/routes/+page.server.ts
+++ bounty-thing/src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import { chatGPT } from "$lib/server/openai";
+import type { ChatCompletion } from "openai/resources/index.mjs";
 
 export async function load() {
-    // const chatGpt = await chatGPT("To be or not to be")
-    return {response: "hi"}
+    // const memes: ChatCompletion = await chatGPT("Say your name")
+    // const idk = memes.choices[0].
+    const memes = "hi"
+    return {memes}
 }
