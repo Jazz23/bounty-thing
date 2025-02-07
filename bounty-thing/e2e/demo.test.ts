aaeb54d209783de681e754ca24b66b4c9ca5d37f
Jazz23 <noreply>
2025-02-07 03:58:19 +0000

    playwright test

--- bounty-thing/e2e/demo.test.ts
+++ bounty-thing/e2e/demo.test.ts
@@ -4,3 +4,9 @@ test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toBeVisible();
 });
+
+test('button called login makes logged in text appear', async ({ page }) => {
+	await page.goto('/');
+	await page.locator('button >> text=/login/i').click()
+	await expect(page.locator('html >> text=/logged in/i')).toBeVisible()
+})
