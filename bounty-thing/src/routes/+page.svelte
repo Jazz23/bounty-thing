ab5428dc725efa4b691fa66b4b96e5869a252103
Jazz23 <noreply>
2025-02-07 04:22:03 +0000

    rename temp

--- bounty-thing/src/routes/+page.svelte
+++ bounty-thing/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Temp from "$lib/temp.svelte";
+	import ChatBubble from "$lib/components/chatBubble.svelte";
     
     let { data } = $props();
 </script>
 
 <div class="bg-gray-200 m-10 flex justify-center">
-    <Temp />
+    <ChatBubble />
 </div>
